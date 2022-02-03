9669939ce27075d100e15b8cccc8f82dab072930
benyoudz <gameriistar284@gmail.com>
2022-02-03 22:11:44 +0100

    Add Inputs.txt For Game

---
+++ Day3/Solve3.py
@@ -0,0 +1,12 @@
+from msilib.schema import Binary
+from typing import BinaryIO
+inputs = open('input.txt','r').readlines()
+
+gammar = 0
+epsilonr=9
+mulGAr=gammar*epsilonr
+
+
+for i in range(len(inputs)):
+    print(input[i])
+
